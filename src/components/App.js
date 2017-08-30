004dbdd0d58bff6e03cc989e3748b53c854a66ce
Buwa Okome <buwa.okome@tlh-m1523.eckoh.com>
2017-08-30 18:01:22 +0100

    implementing reducers

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,27 @@ import * as ReadableAPI from '../utils/api'
 import { connect } from 'react-redux'
 
 class App extends Component {
-  state = {
-    posts: []
-  }
-
+  
   componentDidMount() {
-        ReadableAPI.getAllPosts().then((posts) => {
-            this.setState({posts})
-        })
+    ReadableAPI.getAllPosts().then((posts) => {
+      this.setState({posts})
+    })
   }
 
   render() {
     return (
       <div className="container">
-      {console.log('props', this.props)}
+        {console.log('props', this.props)}
         {console.log('posts', this.state.posts)}
       </div>
     );
   }
 }
 
-export default connect()(App);
+function mapStateToProps (){
+  return {
+
+  }
+}
+
+export default connect(mapStateToProps)(App);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,16 +4,99 @@ import {
   CREATE_POST,
   READ_POST,
   UPDATE_POST,
-  DELETE_POST
+  DELETE_POST,
+  CREATE_COMMENT,
+  READ_COMMENT,
+  UPDATE_COMMENT,
+  DELETE_COMMENT
 } from '../actions'
 
-function post (state = {}, action) {
+function posts (state = {}, action) {
+  const { id, timestamp, title, body, author, category } = action
+
     switch(action.type) {
+        case CREATE_POST:
+          return {
+            ...state,
+
+          }
+
+        case READ_POST:
+          return {
+
+          }
+
+        case UPDATE_POST:
+          return {
+            ...state,
+            [id]: {
+              ...state[id],
+              [title]: title,
+              [body]: body
+            }
+          }
+
+        case DELETE_POST:
+          return {
+              ...state,
+              [id]: {
+                ...state[id],
+                [deleted]: true
+              }
+          }
+
         default :
             return state
     }
 }
 
+function comments (state = {}, action) {
+    switch(action.type) {
+      case CREATE_COMMENT:
+        return {
+
+        }
+
+      case READ_COMMENT:
+        return {
+
+        }
+
+      case UPDATE_COMMENT:
+        return {
+
+        }
+
+      case DELETE_COMMENT:
+        return {
+
+        }
+
+      default:
+        return state
+    }
+}
+
+function votes (state = {}, action) {
+  switch(action.type) {
+    case UPVOTE:
+      return {
+        ...state,
+        [voteScore]: voteScore + 1
+      }
+    case DOWNVOTE:
+      return {
+        ...state,
+        [voteScore]: voteScore - 1
+      }
+
+    default:
+      return state
+  }
+}
+
 export default combineReducers({
-  post
+  posts,
+  comments,
+  votes
 })
